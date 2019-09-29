db6d9ea755a343b5869d7762dc1bb411cf898eda
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-09-29 03:28:54 -0300

    backend login auth working alongside the frontend using passportjs

--- app.js
+++ app.js
@@ -1,68 +1,268 @@
 const express = require("express");
-// const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const AmazonStrategy = require("passport-amazon").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const InstagramStrategy = require("passport-instagram").Strategy;
+const SpotifyStrategy = require("passport-spotify").Strategy;
+const TwitchStrategy = require("passport-twitch.js").Strategy;
+const keys = require("./config/providers");
+const chalk = require("chalk");
 const session = require("express-session");
-const flash = require("connect-flash");
 const process = require("process");
+let user = {};
 
 require("dotenv").config();
-
-// Passport Config
-require("./config/passport")(passport);
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Bodyparser
-app.use(express.urlencoded({ extended: false }));
 
 const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-	console.log("MongoDB Atlas database connection established successfully.");
+  console.log("MongoDB Atlas database connection established successfully.");
 });
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
+// Facebook Strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.FACEBOOK.clientID,
+      clientSecret: keys.FACEBOOK.clientSecret,
+      callbackURL: "/auth/facebook/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Amazon Strategy
+passport.use(
+  new AmazonStrategy(
+    {
+      clientID: keys.AMAZON.clientID,
+      clientSecret: keys.AMAZON.clientSecret,
+      callbackURL: "/auth/amazon/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Github Strategy
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: keys.GITHUB.clientID,
+      clientSecret: keys.GITHUB.clientSecret,
+      callbackURL: "/auth/github/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Google Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE.clientID,
+      clientSecret: keys.GOOGLE.clientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken:", refreshToken);
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Instagram Strategy
+passport.use(
+  new InstagramStrategy(
+    {
+      clientID: keys.INSTAGRAM.clientID,
+      clientSecret: keys.INSTAGRAM.clientSecret,
+      callbackURL: "/auth/instagram/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Spotify Strategy
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: keys.SPOTIFY.clientID,
+      clientSecret: keys.SPOTIFY.clientSecret,
+      callbackURL: "/auth/spotify/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+// Twitch Strategy
+passport.use(
+  new TwitchStrategy(
+    {
+      clientID: keys.TWITCH.clientID,
+      clientSecret: keys.TWITCH.clientSecret,
+      callbackURL: "/auth/twitch/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // Express session
 app.use(
-	session({
-		secret: "secret",
-		resave: true,
-		saveUninitialized: true
-	})
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
 );
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// Routes
+// app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
-// Connect flash
-app.use(flash());
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook"),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
 
-// Global variables
-app.use(function(req, res, next) {
-	res.locals.success_msg = req.flash("success_msg");
-	res.locals.error_msg = req.flash("error_msg");
-	res.locals.error = req.flash("error");
-	next();
+app.get(
+  "/auth/amazon",
+  passport.authenticate("amazon", {
+    scope: ["profile"]
+  })
+);
+app.get(
+  "/auth/amazon/callback",
+  passport.authenticate("amazon"),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+app.get("/auth/github", passport.authenticate("github"));
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github"),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    console.log("Google Profile Info", req.profile);
+    res.redirect("http://localhost:3000/profile");
+  }
+);
+
+app.get("/auth/instagram", passport.authenticate("instagram"));
+app.get(
+  "/auth/instagram/callback",
+  passport.authenticate("instagram"),
+  (req, res) => {
+    res.redirect("http://localhost:3000/profile");
+  }
+);
+
+app.get("/auth/spotify", passport.authenticate("spotify"));
+app.get(
+  "/auth/spotify/callback",
+  passport.authenticate("spotify"),
+  (req, res) => {
+    res.redirect("http://localhost:3000/profile");
+  }
+);
+
+app.get("/auth/twitch", passport.authenticate("twitch.js"));
+app.get(
+  "/auth/twitch/callback",
+  passport.authenticate("twitch.js"),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+app.get("/user", (req, res) => {
+  console.log("getting user data!");
+  console.log("user:", user);
+  res.send(user);
 });
 
-// Routes
-app.use("/", require("./routes/index"));
-app.use("/users", require("./routes/users"));
+app.get("/auth/logout", (req, res) => {
+  console.log("logging out!");
+  user = {};
+  res.redirect("http://localhost:3000");
+});
+
+console.log("Right before PORT 5000");
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
