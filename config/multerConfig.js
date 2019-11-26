4e0765bfb0d9406a4d9ebbc8a2ff8f9bd009d527
seeds <seeds@MacBook-Pro-de-seeds.local>
2019-11-26 17:13:16 -0300

    A different mongoDB atlas was set up.

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -2,22 +2,7 @@ const _ = require("lodash");
 
 const config = require("./globalMulterVariables.json");
 const defaultConfig = config.multer;
-// const environment = process.env.NODE_ENV || "development";
-// const environmentConfig = config[environment];
-// const finalConfig = _.merge(defaultConfig, environmentConfig);
 
-// as a best practice
-// all global variables should be referenced via global. syntax
-// and their names should always begin with g
 global.gConfigMulter = defaultConfig;
-
-// // log global.gConfig
-// console.log(
-//     `global.gConfig: ${JSON.stringify(
-// 		global.gConfig,
-// 		undefined,
-// 		global.gConfig.json_indentation
-// 	)}`
-// );
 
 console.log("global.gConfig:", global.gConfigMulter)
