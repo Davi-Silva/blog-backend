ecc99842975f7bbd632f0ef57f6b4b1c2d6b915b
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-02-06 14:06:54 -0300

    .

---
+++ src/routes/homepage.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
+
+const Post = require('../models/blog/Post');
+const Podcast = require('../models/podcast/Podcast');
+const Course = require('../models/course/Course');
+
+app.get('/get/blog/most-read', async (req, res) => {
+  Post.findOne()
+  .sort('-howManyRead')
+  .then((posts) => {
+    res.status(301).send(posts)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
+
+module.exports = app;
